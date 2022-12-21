0d3559bf83a2a89f2d43e8763f5e14bbee8255c0
Yaroslav Mokhurenko <yaroslav@teamtreehouse.com>
2022-12-21 15:52:46 +0200

    Add cutom event for v4

---
+++ lib/staccato/V4/events/custom_event.rb
@@ -0,0 +1,24 @@
+module Staccato::V4
+  class ViewItemList
+    FIELDS = [
+    ]
+
+
+    def name(name='custom_event')
+      word = name
+      # word.gsub!(/::/, '/')
+      word.gsub!(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+      word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+      word.tr!("-", "_")
+      word.downcase!
+      word.to_sym
+    end
+
+    def fields(fields=nil)
+      fields.to_a
+      FIELDS = fields.to_a
+    end
+
+    include Event
+  end
+end
